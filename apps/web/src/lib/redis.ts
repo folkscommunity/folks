042ef948a31ff74cd51d429881c13c3e7e7608a0
Johny <jan@rokita.me>
2025-02-02 02:25:09 +0100

    build fixes

--- apps/web/src/lib/redis.ts
+++ apps/web/src/lib/redis.ts
@@ -1,3 +1,5 @@
 import Redis from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL!);
+export const redis = new Redis(process.env.REDIS_URL!, {
+  lazyConnect: process.env.REDIS_URL ? false : true
+});
