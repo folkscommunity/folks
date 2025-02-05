a835a625cb4718053ca0112c6c79cfd758042b8b
Junaid Anjum <junaidanjum@live.com>
2025-02-05 22:22:34 +0530

    feat: add inline composer to feeds component

--- apps/web/src/components/composer.tsx
+++ apps/web/src/components/composer.tsx
@@ -619,3 +619,183 @@ export function ReplyComposeFloating({
     </>
   );
 }
+
+// New: This composer is for the inline composer in the feed on the main page.
+export function InlineComposer() {
+  const [text, setText] = useState("");
+
+  const [error, setError] = useState("");
+  const [open, setOpen] = useState(false);
+  const [posting, setPosting] = useState(false);
+
+  const { openFilePicker, filesContent, loading, errors, clear } =
+    useFilePicker({
+      readAs: "DataURL",
+      accept: "image/*",
+      multiple: false,
+      validators: [
+        new FileAmountLimitValidator({ max: 1 }),
+        new FileTypeValidator(["jpg", "jpeg", "png", "webp", "gif"]),
+        new FileSizeValidator({ maxFileSize: 50 * 1024 * 1024 /* 50 MB */ }),
+        new ImageDimensionsValidator({
+          maxWidth: 8000,
+          maxHeight: 8000
+        })
+      ],
+      onFilesRejected: (rejectedFiles: any) => {
+        const reason = rejectedFiles?.errors[0]?.reason;
+
+        if (reason === "FILE_TYPE_NOT_ACCEPTED") {
+          toast.error("Invalid file type.");
+        } else if (reason === "FILE_SIZE_TOO_LARGE") {
+          toast.error("File size exceeds limit.");
+        } else if (reason === "MAX_AMOUNT_OF_FILES_EXCEEDED") {
+          toast.error("You can only upload one file.");
+        } else if (
+          reason === "IMAGE_HEIGHT_TOO_BIG" ||
+          reason === "IMAGE_WIDTH_TOO_BIG"
+        ) {
+          toast.error("Image dimensions exceeds limit. (8000x8000 max)");
+        } else {
+          toast.error("Invalid file type or size.");
+        }
+      }
+    });
+
+  function onPost() {
+    window.dispatchEvent(new Event("refresh_feeds"));
+    window.dispatchEvent(new Event("go_to_everything"));
+  }
+
+  function createPost(body: string, files: any[]) {
+    setPosting(true);
+
+    fetch("/api/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        body: body,
+        files: files
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          setText("");
+          setError("");
+          clear();
+
+          setOpen(false);
+          onPost();
+        } else {
+          setError(res.message || "An error occured.");
+        }
+      })
+      .catch((err) => {
+        setError(err.message || "An error occured.");
+      })
+      .finally(() => {
+        setPosting(false);
+      });
+  }
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  return (
+    <div className="w-full">
+      <textarea
+        ref={textareaRef}
+        className="w-full flex-1 resize-none border-0 bg-transparent px-4 py-1 pt-2 placeholder:text-neutral-700 focus:outline-none"
+        placeholder="Write something..."
+        name="body"
+        maxLength={300}
+        value={text}
+        disabled={posting}
+        onChange={(e) => {
+          setText(e.target.value);
+          e.target.style.height = "auto"; // Reset height
+          e.target.style.height = `${e.target.scrollHeight}px`; // Adjust height
+        }}
+        onFocus={() => setOpen(true)}
+        style={{ overflow: "hidden" }}
+      />
+
+      {posting && (
+        <div className="pointer-events-auto absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center">
+          <CircleNotch className="size-20 animate-spin opacity-50" />
+        </div>
+      )}
+
+      {filesContent.map((file, index) => (
+        <div
+          key={index}
+          className="group relative flex flex-col items-start pl-0 pt-4 max-sm:pl-4"
+        >
+          <img
+            alt={file.name}
+            src={file.content}
+            className="max-h-40 max-w-80 rounded-md border border-slate-300/0 dark:border-slate-800"
+          />
+
+          <button
+            className="bg-black-900 dark:bg-black-800 text-black-100 absolute ml-1 mt-1 rounded-full border border-neutral-300/0 p-1 dark:border-slate-800 dark:text-slate-400"
+            onClick={() => clear()}
+          >
+            <X className="size-4" />
+          </button>
+        </div>
+      ))}
+      {open && (
+        <div className="mt-2 p-4 dark:border-slate-900">
+          <div className="flex items-center justify-between gap-2">
+            <button
+              className="rounded-md text-neutral-700 dark:text-neutral-600"
+              onClick={openFilePicker}
+            >
+              <ImagesSquare size={24} />
+            </button>
+
+            <div
+              className={cn(
+                "font-bold opacity-0 transition-opacity duration-300",
+                text && text.length > 3 && "opacity-100",
+                text && text.length > 290 && "text-orange-500",
+                text && text.length === 300 && "text-red-500"
+              )}
+            >
+              {(text && text.length) || "0"} / 300
+            </div>
+
+            <div className="flex items-center gap-2">
+              <button
+                className="rounded-md border border-neutral-900 px-3 py-1 text-black disabled:border-neutral-400 dark:border-neutral-600 dark:text-white dark:disabled:border-neutral-800"
+                onClick={() => {
+                  setOpen(false);
+                  setText("");
+                  setError("");
+                  clear();
+                  if (textareaRef.current) {
+                    textareaRef.current.style.height = "auto"; // Reset height
+                  }
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                disabled={!text || posting}
+                className="rounded-md bg-neutral-900 px-3 py-1 text-white disabled:bg-neutral-400 dark:bg-neutral-600 dark:disabled:bg-neutral-800"
+                onClick={() => createPost(text, filesContent)}
+              >
+                Post
+              </button>
+            </div>
+          </div>
+          <div className="pt-2">
+            {error && <span className="font-bold text-red-500">{error}</span>}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
