4b16a8625cd1692b317cbadf81dc459eee3bedf7
Johny <jan@rokita.me>
2025-02-27 20:27:21 +0100

    feat: add support page to sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -31,6 +31,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5
+    },
+    {
+      url: "https://folkscommunity.com/support",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5
     }
   ];
 }
