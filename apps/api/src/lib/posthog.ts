2698f2360b777e4563a29c6cb957e8d9876d9e6a
Johny <jan@rokita.me>
2025-05-15 16:34:26 +0200

    fix: update PostHog

--- apps/api/src/lib/posthog.ts
+++ apps/api/src/lib/posthog.ts
@@ -2,10 +2,9 @@ import { PostHog } from "posthog-node";
 
 export const posthog = new PostHog(
   process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-    "phc_5ZLKrqFjOwUgBuAp2IuPgyfcaRGKfAEGyWLygD2mZ1K",
+    "phc_NLlEYiM5V2F5GSArgUp1E30hgLmevdCzKr7v5nTjHxn",
   {
     disabled: process.env.NODE_ENV !== "production",
-    host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://hog.folkscommunity.com"
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com"
   }
 );
--- apps/web/src/components/posthog-provider.tsx
+++ apps/web/src/components/posthog-provider.tsx
@@ -8,11 +8,12 @@ if (typeof window !== "undefined") {
   if (window.location.hostname.includes("folkscommunity.com")) {
     posthog.init(
       process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-        "phc_5ZLKrqFjOwUgBuAp2IuPgyfcaRGKfAEGyWLygD2mZ1K",
+        "phc_NLlEYiM5V2F5GSArgUp1E30hgLmevdCzKr7v5nTjHxn",
       {
         api_host:
           process.env.NEXT_PUBLIC_POSTHOG_HOST ||
-          "https://hog.folkscommunity.com",
+          "https://e.folkscommunity.com",
+        ui_host: "https://us.posthog.com",
         person_profiles: "identified_only"
       }
     );
