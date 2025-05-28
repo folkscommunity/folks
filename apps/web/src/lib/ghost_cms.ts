d7cff44e62e2f00092c962608dc9c050c96d52dc
Johny <jan@rokita.me>
2025-05-28 21:03:50 +0200

    build fix

--- apps/web/src/lib/ghost_cms.ts
+++ apps/web/src/lib/ghost_cms.ts
@@ -1,7 +1,7 @@
 import GhostContentAPI from "@tryghost/content-api";
 
 export const ghost = new GhostContentAPI({
-  url: process.env.GHOST_URL!,
-  key: process.env.GHOST_KEY!,
+  url: process.env.GHOST_URL || "https://folkscommunity.com",
+  key: process.env.GHOST_KEY || "build_fix",
   version: "v5.0"
 });
