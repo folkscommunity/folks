a0f35e9ee4afd5d5453d1447e160cf23c76f677e
Johny <jan@rokita.me>
2025-08-20 22:14:47 +0200

    fix: move images to r2

--- apps/api/src/lib/aws.ts
+++ apps/api/src/lib/aws.ts
@@ -19,10 +19,11 @@ export const ses = new SES({
 });
 
 export const s3 = new S3Client({
-  region: region,
+  region: "auto",
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.R2_ACCESS_KEY_ID,
+    secretAccessKey: process.env.R2_SECRET_ACCESS_KEY
   }
 });
 
